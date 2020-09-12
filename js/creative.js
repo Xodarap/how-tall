faccf47a36c059589c2325cfa494555f6b322d71
Ben West <Xodarap@users.noreply.github.com>
2020-09-11 20:21:34 -0700

    working

--- js/creative.js
+++ js/creative.js
@@ -105,17 +105,13 @@ class Result {
                 var ratio = img.naturalHeight / img.naturalWidth;
                 img.height = img.naturalHeight;
                 img.width = img.naturalWidth;
-                if(ratio < 0.8 || ratio > 1.3) {
-                    $('#dimensionError').show();
-                } else {
-                    $('#dimensionError').hide();
-                }
             });
             img.src = event.target.result;
+            resizeImage($('#photo')[0].files[0], (a,b) => {})
         }
         var files = $('#photo')[0].files;
         if(files && files.length > 0){
-            $('#preview').show();
+            //$('#preview').show();
             reader.readAsDataURL(files[0]);
         } 
     })
@@ -131,13 +127,13 @@ class Result {
         var reader = new FileReader();  
         reader.onload = function(e) {
             img.src = e.target.result;
-            var canvas = document.createElement('canvas');
-            canvas.style.display = 'none';
+            var canvas = $('#myCanvas')[0] //document.createElement('canvas');
+            //canvas.style.display = 'none';
             var ctx = canvas.getContext("2d");
             ctx.drawImage(img, 0, 0);
 
-            var MAX_WIDTH = 102400;
-            var MAX_HEIGHT = 102400;
+            var MAX_WIDTH = 224;
+            var MAX_HEIGHT = 224;
             var width = img.width;
             var height = img.height;
 
@@ -164,6 +160,84 @@ class Result {
         reader.readAsDataURL(file);
     };
 
+    var identifyDrink = function(drinks) {
+        var match = c => drinks.filter(x => x.name == c).length > 0;
+        if(match('beer') && match('bottle')) { return 'beer bottle'; }
+        return 'unkown';
+    }
+    var drawBoxes = function(result) {
+        var regions = result.results[0].outputs[1].data.regions;
+        var canvas = $('#myCanvas')[0]
+        var ctx = canvas.getContext("2d")
+        var personTitles = ['woman', 'man', 'guy', 'person'];
+        var drinkTitles = ['drink', 'icee', 'water', 'bottle', 'can', 'beer'];
+
+        var results = regions.reduce((ac, r) => {
+            var bb = r.region_info.bounding_box;
+            var drinks = r.data.concepts.filter(x => $.inArray(x.name, drinkTitles) >= 0);
+            var people = r.data.concepts.filter(x => $.inArray(x.name, personTitles) >= 0);
+            if(drinks.length > 0 && people.length > 0) {
+                if(drinks[0].value > people[0].value){
+                    people = [];
+                } else {
+                    drinks = [];
+                }
+            }
+            if (drinks.length > 0){
+                console.log('drink')
+                ac.drink = bb.bottom_row - bb.top_row;
+                var length_diff = bb.right_col - bb.left_col;
+                // assume it's horizontal
+                if(length_diff > ac.drink) {
+                    ac.drink = length_diff;
+                }
+                ac.drink_type = identifyDrink(drinks);
+            } else if (people.length > 0){
+                console.log('person')
+                ac.person = bb.bottom_row - bb.top_row;
+            }
+
+            if(drinks.length > 0 || people.length > 0 ){
+                ctx.beginPath();
+                var l = bb.left_col * canvas.width;
+                var r = bb.right_col * canvas.width;
+                var t = bb.top_row * canvas.height;
+                var b = bb.bottom_row * canvas.height;
+                ctx.rect(l, t, r-l, b-t);
+                ctx.lineWidth = 3;
+                ctx.strokeStyle = 'black';
+                ctx.stroke();
+            }
+            return ac;
+        }, {});
+        return results;
+    };
+
+    var heightDifference = function(boxes) {
+        var drink = boxes.drink;
+        var person = boxes.person;
+        var ratio = person / drink;
+        var drink_height = {'beer bottle': 9, 'unknown': 6.25}[boxes.drink_type]
+        var inches = drink_height * ratio;
+        var feet = Math.floor(inches / 12);
+        inches %= 12;
+        return {ratio: ratio, feet: feet, inches: inches, drink_height: drink_height}
+    }
+
+    var displayResults = function(d, r) {
+        if(!isNaN(d.ratio)) {
+            $('#resultText').text(
+                'I believe this person is holding a ' + r.drink_type + ' which I am assuming to be ' + d.drink_height + ' inches tall. ' +
+                'This person is ' + d.ratio.toFixed(2) + ' times taller than the drink, implying a height of ' +
+                d.feet + "'" + d.inches.toFixed(0) + '".'
+            )
+        } else if (r.drink === undefined) {            
+            $('#resultText').text('Error: Could not find a drink');
+        } else if (r.person === undefined) {            
+            $('#resultText').text('Error: Could not find a person');
+        }
+    }
+
     $('#ratingForm').submit(function(event){
         event.preventDefault();
         if(!validate()) {
@@ -175,41 +249,43 @@ class Result {
         var after = function(canvas, result) {
             var data = 
             {
-                "requests": [
-                  {
-                    "image": {
-                      "content": result.replace(/^data:image\/(png|jpg|jpeg|webp);base64,/, "")   
-                    },
-                    "features": [
-                      {
-                        "maxResults": 10,
-                        "type": "OBJECT_LOCALIZATION"
-                      },
-                    ]
+                "inputs": [
+                    {
+                      "data": {
+                        "image": {
+                          "base64": result.replace(/^data:image\/(png|jpg|jpeg|webp);base64,/, "")   
+                      }
+                    }
                   }
                 ]
-              }
+            }
             
             data = JSON.stringify(data);
-            var url = 'https://vision.googleapis.com/v1/images:annotate?key=' + decipher('how-tall')("090132291b310b191f04290d7a11187b2d21013d1c232a7a2711221f0f3c0704210c06001f7c2f")
+            var url = 'https://api.clarifai.com/v2/workflows/food/results'
             $.ajax({url: url, 
                 data: data,
                 type: 'POST',
                 contentType: 'application/json',
                 processData: true,
-                headers: {'content-type': 'application/json'},
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': decipher('how-tall')("032d3168292b2e7d79797b7e7d2b2b2d7c79712c707a2d7d7a2b787a7e797c7a2d7b7c29")
+                },
                 success: function(result){
                     //new Result(result);
-                    var r = result.responses[0].localizedObjectAnnotations;
+                    //var r = result.responses[0].localizedObjectAnnotations;
+                    var r = result.results[0].outputs[1];
+                    var b = drawBoxes(result);
+                    var d = heightDifference(b);
                     $('#loaderHolder').hide();
                     $('#resultsHolder').show();
-                    $('#resultText').text(
-                        r.map(x => x.name).join(',')
-                    )
+                    displayResults(d, b);
                 }   
             });   
         };
         resizeImage(file, after);
     });
+
+    
 
 })(jQuery); // End of use strict
